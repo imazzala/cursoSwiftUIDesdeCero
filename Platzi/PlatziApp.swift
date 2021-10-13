8d23a7e9e7795d7fa681de87c085af3a5a8ea322
imazzala <ivanmazzalay@gmail.com>
2021-10-13 17:18:09 -0300

    Initial Commit

---
+++ Platzi/PlatziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziApp.swift
+//  Platzi
+//
+//  Created by Ivan Ezequiel Mazzalay on 13/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PlatziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
